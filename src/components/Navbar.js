4a7ba81175e2f4db0440c2b8831b93d65ae9ac05
nrasel <nrasel452@gmail.com>
2022-09-12 18:42:33 +0600

    cart item

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const items = useSelector((state) => state.cart);
+
   return (
     <div
       style={{
@@ -18,7 +21,7 @@ const Navbar = () => {
         <Link className="navLink" to="/cart">
           Cart
         </Link>
-        <span className="cartCount">Cart items: 0</span>
+        <span className="cartCount">Cart items: {items.length}</span>
       </div>
     </div>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { remove } from "../store/cartSlice";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.cart);
+  const handleRemove = (productId) => {
+    dispatch(remove(productId));
+  };
+  return (
+    <div>
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+        {products.map((product) => (
+          <div>
+            <div className="cartCard">
+              <img src={product.image} alt="" />
+              <h5>{product.title}</h5>
+              <h5>{product.price}</h5>
+              <button className="btn" onClick={handleRemove(product.id)}>
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
